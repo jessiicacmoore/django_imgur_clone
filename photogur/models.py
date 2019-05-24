32c2d763d7762366159d4c7e0c92a676101233fb
Jessica Moore <jessica.cmoore1@gmail.com>
2019-05-24 12:44:27 -0400

    Created class of Picture

---
+++ photogur/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Picture(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)

eb530f45eafdce2707b56e1fe492bf92cc9bfb2a
Jessica Moore <jessica.cmoore1@gmail.com>
2019-05-31 12:38:50 -0400

    Login form working

--- photogur/views.py
+++ photogur/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views.decorators.http import require_http_methods
+from django.contrib.auth import authenticate, login
+
 
 from photogur.models import Picture, Comment
 from photogur.forms import LoginForm
@@ -49,7 +51,20 @@ def create_comment(request):
 
 
 def login_view(request):
-    form = LoginForm()
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            pw = form.cleaned_data['password']
+            user = authenticate(username=username, password=pw)
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect('/pictures')
+            else: 
+                form.add_error('username', 'Login failed')
+    else:
+        form = LoginForm()
+
     context = { 'form': form }
     http_response = render(request, 'login.html', context)
     return HttpResponse(http_response)
